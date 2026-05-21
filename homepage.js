9db5b28910fe654152a679b662810e60f7b6b965
Nitesh Meena <149501086+Niteshmeena01@users.noreply.github.com>
2026-05-21 15:41:18 +0530

    inital commit

---
+++ homepage.js
@@ -0,0 +1,34 @@
+const menuBtn = document.getElementById("menu-btn");
+const navbar = document.getElementById("navbar");
+
+menuBtn.onclick = () => {
+  navbar.classList.toggle("active");
+};
+
+/* Close menu on link click */
+
+document.querySelectorAll(".navbar a").forEach(link => {
+
+  link.addEventListener("click", () => {
+    navbar.classList.remove("active");
+  });
+
+});
+
+/* Navbar shadow on scroll */
+
+window.addEventListener("scroll", () => {
+
+  const header = document.querySelector(".header");
+
+  if(window.scrollY > 50){
+    header.style.boxShadow =
+    "0 5px 15px rgba(0,0,0,0.15)";
+  }
+
+  else{
+    header.style.boxShadow =
+    "0 2px 10px rgba(0,0,0,0.1)";
+  }
+
+});
